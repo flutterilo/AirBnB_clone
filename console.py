147ceddd1f9cc3e7977665fa18e4266455a4eec4
Ilyass <ilias.kassimi@gmail.com>
2023-08-13 15:04:38 +0000

    airbnb files

--- console.py
+++ console.py
@@ -1,39 +1,320 @@
 #!/usr/bin/python3
-"""
-AirBnB clone
-"""
+
+"""AirBnB Clone - Command Line Interface (CLI) Module"""
+
 import cmd
-import sys
 from models.base_model import BaseModel
+from models.place import Place
+from models.review import Review
+from models.user import User
+from models.state import State
+from models import storage
+from models.city import City
+from models.amenity import Amenity
+import json
+import shlex
 
 
 class HBNBCommand(cmd.Cmd):
-    """Class definition HBNB Command"""
-    prompt = "(hbnb)"
+    """command prompt to access models data """
+    prompt = '(hbnb) '
+    class_names = {
+        "BaseModel": BaseModel,
+        "State": State,
+        "City": City,
+        "Amenity": Amenity,
+        "Place": Place,
+        "User": User,
+        "Review": Review
+    }
+
+    def default(self, arg):
+        """
+        Called on an input line when the command prefix is not recognized.
+        If this method is not overridden, it prints an error message and
+        returns.
+        """
+        methods_dict = {
+            "all": self.do_all,
+            "count": self.do_count,
+            "show": self.do_show,
+            "destroy": self.do_destroy,
+            "update": self.do_update
+        }
+        commands = arg.strip().split(".")
+        if len(commands) != 2:
+            cmd.Cmd.default(self, arg)
+            return
+        class_name = commands[0]
+        command = commands[1].split("(")[0]
+        line = ""
+        if (command == "update" and commands[1].split("(")[1][-2] == "}"):
+            inputs_data = commands[1].split("(")[1].split(",", 1)
+            inputs_data[0] = shlex.split(inputs_data[0])[0]
+            line = "".join(inputs_data)[0:-1]
+            line = class_name + " " + line
+            self.do_update_using_class(line.strip())
+            return
+        try:
+            inputs_data = commands[1].split("(")[1].split(",")
+            for n in range(len(inputs_data)):
+                if (n != len(inputs_data) - 1):
+                    line = line + " " + shlex.split(inputs_data[n])[0]
+                else:
+                    line = line + " " + shlex.split(inputs_data[n][0:-1])[0]
+        except IndexError:
+            inputs_data = ""
+            line = ""
+        line = class_name + line
+        if (command in methods_dict.keys()):
+            methods_dict[command](line.strip())
 
     def do_quit(self, arg):
-        """Exit the program with the Quit command"""
+        """
+        Quit command to exit the program
+
+        Usage: quit
+
+        Return:
+            True
+        """
         return True
 
     def do_EOF(self, arg):
-        """Exit the program on EOF"""
-        print()
+        """
+        EOF command to exit the program when the user inputs EOF (Ctrl+D)
+
+        Usage: EOF or CTRL+D
+
+        Return:
+            True
+        """
+        print("")
         return True
 
     def emptyline(self):
-        """To do nothing when an empty line is the entry."""
+        """
+        Called when an empty line is entered in response to the prompt.
+
+        If this method is not overridden, it repeats the last nonempty
+        command entered.
+
+        Return:
+            None
+        """
+        pass
+
+    def do_nothing(self, arg):
+        """ Does nothing """
         pass
 
     def do_create(self, arg):
-        """Create method that creats an object"""
-        if arg is None:
+        """
+        Creates a new instance of BaseModel, saves it (to the JSON file)
+        and prints the id
+
+        Usage: create <class name>
+
+        Args:
+            arg (str): class name of the instance to create
+
+        Return:
+            None
+        """
+        if not arg:
             print("** class name missing **")
-        elif hasattr(sys.modules[__name__], arg):
-            cls = getattr(sys.modules[__name__], arg)
-            instance = cls()
-            print(instance.id)
+            return
+        line = shlex.split(arg)
+        if line[0] not in HBNBCommand.class_names.keys():
+            print("** class doesn't exist **")
+            return
+        new_inst = HBNBCommand.class_names[line[0]]()
+        new_inst.save()
+        print(new_inst.id)
+
+    def do_show(self, arg):
+        """
+        Prints the string representation of an instance based on the class
+        name and id
+
+        Usage: show <class name> <id>
+
+        Args:
+            arg (str): class name and id of the instance to show
+
+        Return:
+            None
+        """
+        command_args = shlex.split(arg)
+        if len(command_args) == 0:
+            print("** class name missing **")
+            return
+        if command_args[0] not in HBNBCommand.class_names.keys():
+            print("** class doesn't exist **")
+            return
+        if len(command_args) <= 1:
+            print("** instance id missing **")
+            return
+        inst_dicts = storage.all()
+        key = "{}.{}".format(command_args[0], command_args[1])
+        if key in inst_dicts:
+            obj_instance = str(inst_dicts[key])
+            print(obj_instance)
+        else:
+            print("** no instance found **")
+
+    def do_destroy(self, arg):
+        """
+        Deletes an instance based on the class name and id (save the change
+        into the JSON file)
+
+        Usage: destroy <class name> <id>
+
+        Args:
+            arg (str): class name and id of the instance to destroy
+
+        Return:
+            None
+        """
+        command_args = shlex.split(arg)
+        if len(command_args) == 0:
+            print("** class name missing **")
+            return
+        if command_args[0] not in HBNBCommand.class_names.keys():
+            print("** class doesn't exist **")
+            return
+        if len(command_args) <= 1:
+            print("** instance id missing **")
+            return
+        inst_dicts = storage.all()
+        key = "{}.{}".format(command_args[0], command_args[1])
+        if key in inst_dicts:
+            del inst_dicts[key]
+            storage.save()
+        else:
+            print("** no instance found **")
+
+    def do_all(self, arg):
+        """
+        Prints all string representation of all instances based or not on the
+        class name
+
+        Usage: all <class name> or all
+
+        Args:
+            arg (str): class name of the instance to show or nothing
+        """
+        json_data = []
+        inst_dicts = storage.all()
+        if not arg:
+            for key in inst_dicts:
+                json_data.append(str(inst_dicts[key]))
+            print(json.dumps(json_data))
+            return
+        command_arg = shlex.split(arg)
+        if command_arg[0] in HBNBCommand.class_names.keys():
+            for key in inst_dicts:
+                if command_arg[0] in key:
+                    json_data.append(str(inst_dicts[key]))
+            print(json.dumps(json_data))
         else:
             print("** class doesn't exist **")
+
+    def do_update(self, arg):
+        """
+        Updates an instance based on the class name and id by adding or
+        updating attribute (save the change into the JSON file)
+
+        Usage: update <class name> <id> <attribute name> "<attribute value>"
+
+        Args:
+            arg (str): class name, id, attribute name and attribute value of
+            the instance to update
+        """
+        if not arg:
+            print("** class name missing **")
+            return
+        command_arg = shlex.split(arg)
+        insts_dicts = storage.all()
+        if command_arg[0] not in HBNBCommand.class_names.keys():
+            print("** class doesn't exist **")
+            return
+        if (len(command_arg) == 1):
+            print("** instance id missing **")
+            return
+        try:
+            key = command_arg[0] + "." + command_arg[1]
+            insts_dicts[key]
+        except KeyError:
+            print("** no instance found **")
+            return
+        if (len(command_arg) == 2):
+            print("** attribute name missing **")
+            return
+        if (len(command_arg) == 3):
+            print("** value missing **")
+            return
+        inst = insts_dicts[key]
+        if hasattr(inst, command_arg[2]):
+            data_type = type(getattr(inst, command_arg[2]))
+            setattr(inst, command_arg[2], data_type(command_arg[3]))
+        else:
+            setattr(inst, command_arg[2], command_arg[3])
+        storage.save()
+
+    def do_update_using_class(self, arg):
+        """
+        Updates an instance based on the class name and id by adding or
+        updating attribute (save the change into the JSON file)
+        """
+        if not arg:
+            print("** class name missing **")
+            return
+        dictionary_data = "{" + arg.split("{")[1]
+        data_arg = shlex.split(arg)
+        objs_dict = storage.all()
+        if data_arg[0] not in HBNBCommand.class_names.keys():
+            print("** class doesn't exist **")
+            return
+        if (len(data_arg) == 1):
+            print("** instance id missing **")
+            return
+        try:
+            key = data_arg[0] + "." + data_arg[1]
+            objs_dict[key]
+        except KeyError:
+            print("** no instance found **")
+            return
+        if (dictionary_data == "{"):
+            print("** attribute name missing **")
+            return
+
+        dictionary_data = dictionary_data.replace("\'", "\"")
+        dictionary_data = json.loads(dictionary_data)
+        inst = objs_dict[key]
+        for my_key in dictionary_data:
+            if hasattr(inst, my_key):
+                data_type = type(getattr(inst, my_key))
+                setattr(inst, my_key, dictionary_data[my_key])
+            else:
+                setattr(inst, my_key, dictionary_data[my_key])
+        storage.save()
+
+    def do_count(self, arg):
+        """
+        Retrieve the number of instances of a class
+
+        Usage: <class name>.count()
+
+        Args:
+            arg (str): class name of the instance to count
+        """
+        counter = 0
+        objects_dict = storage.all()
+        for key in objects_dict:
+            if (arg in key):
+                counter += 1
+        print(counter)
 
 
 if __name__ == '__main__':
--- models/base_model.py
+++ models/base_model.py
@@ -1,42 +1,99 @@
 #!/usr/bin/python3
-"""
-AirBnB clone - The console
-"""
-import uuid
-from datetime import datetime
+
+"""BaseModel class module"""
+
 import models
+
+from uuid import uuid4
+from datetime import datetime
+
+DATE_ISO8601_FORMAT = "%Y-%m-%dT%H:%M:%S.%f"
 
 
 class BaseModel:
+    """
+    BaseModel class
 
+    Attributes:
+        id (str): unique id generated for the BaseModel instance
+        created_at (datetime): datetime object representing the time the
+        BaseModel instance was created
+        updated_at (datetime): datetime object representing the time the
+        BaseModel instance was updated
+
+    Methods:
+        __init__(*args, **kwargs): initializes a BaseModel instance
+        __str__(): returns the string representation of the BaseModel instance
+        save(): updates the updated_at attribute with the current datetime
+        to_dict(): returns a dictionary containing all keys/values of the
+        BaseModel instance
+    """
     def __init__(self, *args, **kwargs):
+        """
+        Initializes a BaseModel instance
+
+        Args:
+            *args: unused
+            **kwargs: keyword arguments passed to the constructor
+
+        Keyword Args:
+            id (str): unique id generated for the BaseModel instance
+            created_at (datetime): datetime object representing the time the
+            BaseModel instance was created
+            updated_at (datetime): datetime object representing the time the
+            BaseModel instance was updated
+
+        Returns:
+            None
+        """
         if kwargs:
-            time_format = '%Y-%m-%dT%H:%M:%S.%f'
-            for key, val in kwargs.items():
-                if key == '__class__':
-                    continue
-                elif key == 'created_at' or key == 'updated_at':
-                    setattr(self, key, datetime.strptime(val, time_format))
-                else:
-                    setattr(self, key, val)
+            for key in kwargs:
+                if key != "__class__":
+                    setattr(self, key, kwargs[key])
+                if key == "created_at" or key == "updated_at":
+                    setattr(self, key, datetime.strptime(
+                        kwargs[key], DATE_ISO8601_FORMAT))
         else:
-            self.id = str(uuid.uuid4())
+            self.id = str(uuid4())
             self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.updated_at = self.created_at
+
+            # Add a call to the new(self) method on storage
             models.storage.new(self)
 
     def __str__(self):
-        """A string representation of an object using the method __str__"""
-        return (f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
+        """
+        Returns the string representation of the BaseModel instance"
+
+        Returns:
+            str: string representation of the BaseModel instance
+        """
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id,
+                                     self.__dict__)
 
     def save(self):
-        """An update to the attribute updated_at using the method save"""
+        """
+        Updates the updated_at attribute with the current datetime
+
+        Returns:
+            None
+        """
         self.updated_at = datetime.now()
-        models.storage.save()
+        models.storage.save()  # Call the save() method of the storage instance
 
     def to_dict(self):
-        """A dictionary representation of an object using the method to_dict"""
-        dictionnary = self.__dict__.copy()
-        dictionnary["__class__"] = self.__class__.__name__dictionnary["created_at"] = self.updated_at.isoformat()
-        dictionnary["updated_at"] = self.updated_at.isoformat()
-        return dictionnary
+        """
+        Returns a dictionary containing all keys/values of the BaseModel
+        instance
+
+        Returns:
+            dict: dictionary containing all keys/values of the BaseModel
+            instance
+        """
+        obj_dict = self.__dict__.copy()
+
+        obj_dict["__class__"] = self.__class__.__name__
+        obj_dict["created_at"] = self.created_at.isoformat()
+        obj_dict["updated_at"] = self.updated_at.isoformat()
+
+        return obj_dict
